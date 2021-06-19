6750310b8e2a153e7c604db68becefa96a7c7bb5
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-06-19 18:06:06 +0530

    Add files via upload

---
+++ add3number.java
@@ -0,0 +1,20 @@
+
+package codewithaman;
+
+import java.util.Scanner;
+
+//java.util.Scanner;
+public class add3number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		   System.out.println("enter 3 number");
+		   int a=sc.nextInt();
+		   int b=sc.nextInt();
+		   int c=sc.nextInt();
+int sum=a+b+c;
+System.out.println("sum of all enterred number is "+sum);
+	}
+
+}
